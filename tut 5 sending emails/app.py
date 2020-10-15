98d0365493d0f451b6638c443d150f97d9197eda
skagnihotri <skagnihotri1@gmail.com>
2020-10-15 19:16:43 +0530

    tut 4 cmplt

--- tut 5 sending emails/app.py
+++ tut 5 sending emails/app.py
@@ -1,7 +1,22 @@
 from flask import Flask, request, render_template, redirect
+import send_mail
 
 app = Flask(__name__)
 
+@app.route('/')
+def home() :
+	return render_template("index.html")
 
+@app.route('/', methods = ['POST'])
+def home_submit() :
+	sender = request.form['from']
+	reciever = request.form['to']
+	subject = request.form['subject']
+	message = request.form['message']
+	password = request.form['password']
+	
+	send_mail.email(sender, reciever, subject, message, password)
+	return render_template("index.html", done = True)
+	
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run()
--- tut 5 sending emails/send_mail.py
+++ tut 5 sending emails/send_mail.py
@@ -1,27 +1,20 @@
 # Library
 import smtplib
 
-def send_mail(sender, reciever) :
-	# creates SMTP session 
+def email(sender, reciever, subject, message, password) :
+	final_msg = f"Subject: {subject}\n\n{message}"
+
+	# creates SMTP session
 	session = smtplib.SMTP('smtp.gmail.com', 587) 
 	
 	# start TLS for security 
 	session.starttls() 
 	
-	# message to be sent 
-	message = 'from python mail'
-	
 	# Authentication
-	password = input("Enter your password : ")
 	session.login(sender, password)
 
 	# sending the mail
-	session.sendmail(sender, reciever, message) 
+	session.sendmail(sender, reciever, final_msg)
 	
 	# terminating the session 
 	session.quit()
-
-if __name__ == '__main__':
-	sender = 'smagni1002@gmail.com'
-	reciever = 'chhabraashish123@gmail.com '
-	send_mail(sender, reciever)
